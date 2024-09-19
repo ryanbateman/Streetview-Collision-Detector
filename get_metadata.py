1e8ffedfbe86173752e2a8da785a3fdba3ebdce3
Ryan Bateman <secretdark@gmail.com>
2024-09-19 16:56:17 +0200

    Got it working but now requests require chunking

--- get_metadata.py
+++ get_metadata.py
@@ -1,7 +1,8 @@
-import requests_cache, os
+import os
 import urllib.parse
 import logging as log
-from datetime import datetime, date
+from aiohttp import ClientSession
+from datetime import datetime
 from google_takeout_parser.models import PlaceVisit
 
 # setup
@@ -9,37 +10,43 @@ meta_base = 'https://maps.googleapis.com/maps/api/streetview/metadata?'
 pic_base = 'https://maps.googleapis.com/maps/api/streetview?'
 api_key = os.environ.get('GMAPS_STATIC_API_KEY')
 
-# setup request
-session = requests_cache.CachedSession('maps_cache', expire_after=3600)
-
-def checkPlaceVisit(placeVisit: PlaceVisit):
+async def checkPlaceVisit(placeVisit: PlaceVisit):
 	# Eliminate the check when the place visit is out of hours
 	if placeVisit.dt.hour > 17 and placeVisit.dt.hour < 7:
+		log.debug("Discarded")
 		return
-	# obtain the metadata of the request
-	meta_params = {'key': api_key, 'location': f"{placeVisit.lat},{placeVisit.lng}"}
-	meta_response = session.get(meta_base, params=meta_params)
-
-	# Parse response
-	response_json = meta_response.json()
-	if meta_response.status_code != 200:
-		log.error(f"Failed API call {meta_response.status_code}")
-		log.error(response_json)
-		return
-	elif response_json['status'] == "ZERO_RESULTS":
-		return
-	elif response_json['status'] == "UNKNOWN_ERROR" or response_json['status'] == "REQUEST_DENIED":
-		log.error(f"Error with request: {response_json['status']}")
-		return
-	else: 	
-		date = datetime.strptime(response_json['date'], "%Y-%m")
-		if date.month == placeVisit.dt.month and date.year == placeVisit.dt.year:
-			print("----")
-			print(f"Collision! Your visit was within the month of Streetview visits for {placeVisit.location['address']}")
-			print(f"Streetview date: {date.year}-{date.month}")
-			print(f"You visited date: {placeVisit.dt}")
-			params = urllib.parse.urlencode({'query_place_id': f"{[placeVisit.location['placeId']]}", 'query': f"{placeVisit.lat},{placeVisit.lng}"})
-			print(f"Maps: https://www.google.com/maps/search/?api=1&{params}")
-			print("----")
-			print("")
-		
+	# setup request	
+	async with ClientSession() as session:
+		# obtain the metadata of the request
+		meta_params = {'key': api_key, 'location': f"{placeVisit.lat},{placeVisit.lng}"}
+		log.debug("Processing...")
+		async with session.get(meta_base, params=meta_params) as meta_response:
+			# Parse response
+			log.debug(f"Returned: {meta_response.status}")
+			response_json = await meta_response.json()
+			if meta_response.status != 200:
+				log.error(f"Failed API call {meta_response.status_code}")
+				log.error(response_json)
+				return
+			elif response_json['status'] == "ZERO_RESULTS":
+				log.info("No results")
+				return
+			elif response_json['status'] == "UNKNOWN_ERROR" or response_json['status'] == "REQUEST_DENIED":
+				log.error(f"Error with request: {response_json['status']}")
+				return
+			else: 	
+				date = datetime.strptime(response_json['date'], "%Y-%m")
+				if date.month == placeVisit.dt.month and date.year == placeVisit.dt.year:
+					log.info("Hit!")
+					log.info("----")
+					log.info(f"Collision! Your visit was within the month of Streetview visits for {placeVisit.location['address']}")
+					log.info(f"Streetview date: {date.year}-{date.month}")
+					log.info(f"You visited date: {placeVisit.dt}")
+					params = urllib.parse.urlencode({'query_place_id': f"{[placeVisit.location['placeId']]}", 'query': f"{placeVisit.lat},{placeVisit.lng}"})
+					log.info(f"Maps: https://www.google.com/maps/search/?api=1&{params}")
+					log.info("----")
+					log.info("")
+				else:
+					log.debug("Miss")
+				return
+			
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import get_metadata, parse_takeout
 import logging as log
-from tqdm import tqdm
+import asyncio
+from tqdm.asyncio import tqdm
 
+log.basicConfig(level=log.INFO)
 log.info("Starting search on streetview...")
-placeVisits = parse_takeout.getPlaceVisits()
-for placeVisit in tqdm(placeVisits):
-    get_metadata.checkPlaceVisit(placeVisit)
+
+async def main():
+    placeVisits = parse_takeout.getPlaceVisits()
+    tasklist = []
+    log.info("Starting place visit check")
+    for placeVisit in placeVisits[0:1000]:
+        tasklist.append(asyncio.create_task(get_metadata.checkPlaceVisit(placeVisit)))
+    pbar = tqdm(asyncio.as_completed(tasklist), total=len(tasklist))
+    res = [await t for t in pbar]
+
+asyncio.run(main())
